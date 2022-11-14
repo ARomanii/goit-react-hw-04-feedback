f06c812f88141677629bb4d3b64189a554490c9d
Alona <103571003+ARomanii@users.noreply.github.com>
2022-11-14 02:02:49 +0200

    ДЗ 4. ч.1 пробы

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,27 +1,24 @@
 import PropTypes from 'prop-types';
 import './Feedback.module.css';
 
-export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-    const stateArr = Object.keys(options);
-    // console.log(stateArr);
-  
-    return (
-      <div>
-        {stateArr.map(option => (
-          <button
-            key={option}
-            type="button"
-            name={option}
-            onClick={() => onLeaveFeedback(option)}
-          >
-            {option}
-          </button>
-        ))}
-      </div>
-    );
-  };
-  
-  FeedbackOptions.propTypes = {
-    options: PropTypes.object,
-    onLeaveFeedback: PropTypes.func,
-  };
+export const FeedbackOptions = ({ options, updateFeedback }) => {
+  return (
+    <div>
+      {options.map(option => (
+        <button
+          key={option}
+          type="button"
+          name={option}
+          onClick={updateFeedback}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array,
+  updateFeedback: PropTypes.func,
+};
